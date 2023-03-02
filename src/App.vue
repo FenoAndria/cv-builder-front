258d9487462ff153cde28e4b6b19ff6186536343
FenoAndria <feno.andriamorasata@gmail.com>
2023-03-02 15:03:44 +0100

    UI Layout: Navbar, Main Container

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
-<script >
-export default {
-  name: "APP",
-};
-</script>
 
 <template>
-
+  <div class=" min-h-screen">
+    <Navbar />
+    <div class="container py-2 px-32">
+      <MainCard>
+        <router-view />
+        <h1>COUCOU</h1>
+      </MainCard>
+    </div>
+  </div>
 </template>
 
+<script >
+import Navbar from "./components/Layouts/Navbar.vue";
+import MainCard from "./components/Layouts/MainCard.vue";
+export default {
+  name: "APP",
+  components: {
+    Navbar,
+    MainCard,
+  },
+};
+</script>
 <style scoped>
 </style>
---
+++ src/components/Layouts/MainCard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="bg-white p-8 rounded-lg shadow-md drop-shadow-md">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MainCard'
+}
+</script>
